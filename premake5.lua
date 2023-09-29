c6a9d38bfe453c14d76e6d39445b60ce0223fd75
Yanyifan Liao <ben.jaminlyyf21@gmail.com>
2023-09-29 06:57:12 +0200

    Add premake5 support

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "MythEngine"
+  architecture "x64"
+  startproject "Sandbox"
+
+  configurations
+  {
+    "Debug",
+    "Release",
+    "Dist"
+  }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Myth"
+  location "Myth"
+  kind "SharedLib"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs
+  {
+    "%{prj.name}/vendor/spdlog/include"
+  }
+
+  filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "latest"
+
+    defines
+    {
+      "MYTH_PLATFORM_WINDOWS",
+      "MYTH_BUILD_DLL"
+    }
+
+    postbuildcommands
+    {
+      ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+  filter "configurations:Debug"
+    defines "MYTH_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "MYTH_RELEASE"
+    optimize "On"
+
+  filter "configurations:Dist"
+    defines "MYTH_DIST"
+    optimize "On"
+
+project "Sandbox"
+  location "Sandbox"
+  kind "ConsoleApp"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs
+  {
+    "Myth/vendor/spdlog/include",
+    "Myth/src"
+  }
+
+  links
+  {
+    "Myth"
+  }
+
+  filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "latest"
+
+    defines
+    {
+      "MYTH_PLATFORM_WINDOWS"
+    }
+
+  filter "configurations:Debug"
+    defines "MYTH_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "MYTH_RELEASE"
+    optimize "On"
+
+  filter "configurations:Dist"
+    defines "MYTH_DIST"
+    optimize "On"
